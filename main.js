11aa8f198049588691576f0f9d77059a4abde582
Jeremy Barbara <jeremy.w.barbara@gmail.com>
2022-08-17 16:24:44 -0400

    commit tree d445e9da331a3fc31eb0ac17cb5daefc7fd56160

--- main.js
+++ main.js
@@ -3,10 +3,35 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+function getLikeButtons(){
+  const likeButtons = document.querySelectorAll(".like-glyph")
+  likeButtons.forEach((button) => button.addEventListener('click', handleClick))
 
+}
 
+function handleClick(event){
+  mimicServerCall()
+    .then((resp) => {
+      if(event.target.className === 'like-glyph'){
+        event.target.innerText = FULL_HEART
+        event.target.className = 'activated-heart'
+      }else{
+        event.target.innerText = EMPTY_HEART
+        event.target.className = 'like-glyph'
+      }
+    })
+    .catch((error) => {
+      const errorModal = document.querySelector('#modal')
+      const errorMessage = document.createElement('p')
+      errorMessage.innerHTML = error
+      errorModal.appendChild(errorMessage)
+      errorModal.className = ''
+      setTimeout((() => errorModal.className = 'hidden'), 3000)
 
+    })
+}
 
+getLikeButtons()
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
